ac5b29899b353cf01a41da3776d33732522eaa86
laurindo <dansymbols@gmail.com>
2019-02-11 00:03:44 -0300

    Finished web development task

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import configureStore from '../config/configureStore';
+import React from "react";
+import { Provider } from "react-redux";
 
-import Logo from '../components/Logo';
-import BalanceSliderContainer from './BalanceSliderContainer';
-import ConvertedCurrencyContainer from './ConvertedCurrencyContainer';
-import ContainerAnimated from '../components/ContainerAnimated';
+import configureStore from "../config/configureStore";
+import Logo from "../components/Logo";
+import BalanceSliderContainer from "./BalanceSliderContainer";
+import ConvertedCurrencyContainer from "./ConvertedCurrencyContainer";
+import ContainerAnimated from "../components/ContainerAnimated";
 
 const store = configureStore();
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="index-page">
-        <Provider store={store}>
-          <ContainerAnimated>
-            <Logo width='20%' height='20%' />
-            <BalanceSliderContainer title="Balances" />
-            <ConvertedCurrencyContainer />
-          </ContainerAnimated>
-        </Provider>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="index-page">
+    <Provider store={store}>
+      <ContainerAnimated>
+        <Logo width="20%" height="20%" />
+        <BalanceSliderContainer title="Balances" />
+        <ConvertedCurrencyContainer />
+      </ContainerAnimated>
+    </Provider>
+  </div>
+);
+
+export default App;
